bacd75391caa8bcecb1ed2b18643909311c71cef
ZakariaBatty <zbatty1297@gmail.com>
2024-05-26 15:54:31 +0100

    header and hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        'md-bottom': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      },
+      colors: {
+        primary: "#08D9D6"
+      }
     },
   },
   plugins: [],
